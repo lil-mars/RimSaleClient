07ecfdaacda7e1b0c54208d95ef4d3cfbc51bd8b
v10lador <ermateze@gmail.com>
2019-11-24 22:25:09 -0400

    Revert "Creando modulo de reportes para el administrador"
    This reverts commit b48a62911d1aeeeab397940aa3ca7fb5c7e09cab.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,27 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { FullCalendarModule } from '@fullcalendar/angular';
 import { NavComponent } from './components/nav/nav.component';
-import { ReportsComponent } from './components/reports/reports.component';
-import { CalendarComponent } from './components/reports/calendar/calendar.component';
-import { ReportsSummaryComponent } from './components/reports/reports-summary/reports-summary.component';
-import { OthersComponent } from './components/reports/others/others.component';
 
 
 @NgModule({
-  declarations: [
-    NavComponent,
-    ReportsComponent,
-    CalendarComponent,
-    OthersComponent,
-    ReportsSummaryComponent,
-  ],
+  declarations: [NavComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule,
-    FullCalendarModule
+    AdminRoutingModule
   ]
 })
-export class AdminModule {
-}
+export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { RouterModule } from '@angular/router';
+import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -18,5 +19,4 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
